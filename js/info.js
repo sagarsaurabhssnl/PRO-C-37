c47e4451e058251bfbf56b139859da66ed9fffa9
sagarsaurabhssnl <56288082+sagarsaurabhssnl@users.noreply.github.com>
2021-01-19 10:29:27 +0530

    Update info.js

--- js/info.js
+++ js/info.js
@@ -12,13 +12,12 @@ class Info {
         text("B to make him sleep on the bed", 65, 60);
         text("D to think 'if he dies'", 65, 80);
         text("G to take him to garden", 65, 100);
-        text("H to have your pet vaccination", 65, 120);
+        text("W to take him to washroom", 65, 120);
         text("L to see when he is lazy", 65, 140);
         text("S to tak ehim to living room", 65, 160);
         text("RIGHT ARROW to make him run right", 65, 180);
         text("LEFT ARROW to make him run left", 65, 200);
         text("V to see the vacation shedule", 65, 220);
-        text("W to take him to washroom", 65, 240);
     }
 
     hide() {
